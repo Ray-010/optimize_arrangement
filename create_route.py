fbabb8d7a8c627f56813eb1f3464eff107e84e5f
Ray-010 <Ray010@outlook.jp>
2022-10-10 18:51:25 +0900

    create pole way

--- create_route.py
+++ create_route.py
@@ -8,9 +8,13 @@ class CreateRoute():
         self.area_height = height
         self.start = [sy, sx]
         self.goal = [gy, gx]
+
+        # 経路マップ
         self.area = [[-1]*self.area_width for _ in range(self.area_height)]
         # 探索済み座標のメモ
         self.visualize_area = [["."]*self.area_width for _ in range(self.area_height)]
+        # ポール座標・経路・ベルト接続を含んだ拡張マップ
+        self.pole_area = [[" " for _ in range(self.area_width*2+1)] for _ in range(self.area_height*2+1)]
     
     def bfs_shortest_distance(self, start: list, goal:list) -> None:
         sy, sx = start
@@ -56,21 +60,10 @@ class CreateRoute():
         tmp.append(self.start)
 
         for i in range(passing_point_num):
-            # if "#" in self.visualize_area[i] and not congestion_rate==3:
-            #     step += 2
-            # else:
-            
             if LR == "right":
                 if self.goal[0] <= step:
                     break
                 y, x = step, self.area_width-1
-                
-                # 障害物に当たるかを確認
-                # if self.visualize_area[y][x] == "#":
-                #     for x2 in range(x, 0, -1):
-                #         if not (self.visualize_area[y][x2] == "#" or self.visualize_area[y+1][x2] == "#"):
-                #             x = x2
-                #             break
 
                 tmp.append([y, x])
                 LR = "left"
@@ -78,12 +71,6 @@ class CreateRoute():
                 if self.goal[0] <= step:
                     break
                 y, x = step, 0
-                # 障害物に当たるかを確認
-                # if self.visualize_area[y][x] == "#":
-                #     for x2 in range(1, self.area_width-1):
-                #         if not (self.visualize_area[y][x2] == "#" or self.visualize_area[y+1][x2] == "#"):
-                #             x = x2
-                #             break
                 
                 tmp.append([y, x])
                 LR = "right"
@@ -133,69 +120,151 @@ class CreateRoute():
                 self.area[y][x] = -1
         
     # 障害物生成
-    def create_obstacle(self, num):
-        tmp = []
+    def create_obstacle(self, num) -> None:
+        obstacles = []
         for _ in range(num):
             y, x = random.randrange(self.area_height), random.randrange(self.area_width)
             if y == self.start[0] and x == self.start[1]:
                 continue
             if y == self.goal[0] and x == self.goal[1]:
                 continue
-            tmp.append([y, x])
+            obstacles.append([y, x])
             self.visualize_area[y][x] = "#"
             
-        print("Obstacles: ", *tmp)
+        print("Obstacles: ", *obstacles)
 
-    def create_pole_arrangement(self) -> list:
-        width = self.area_width*2+1
-        height = self.area_height*2+1
-        pole_area = [[" " for _ in range(width)] for _ in range(height)]
-
-        tmp = []
-        
+    # ポール座標生成
+    def create_pole_coordinates(self) -> list:
+        poles = []
+        idx = 0
         for i in range(self.area_height):
             for j in range(self.area_width):
+                # 未探索なら
                 if self.area[i][j] != -1:
                     i2 = i*2+1
                     j2 = j*2+1
-                    pole_area[i2][j2] = self.area[i][j]
-                    
-                    # 次の座標の方向を保存
-                    neighbor_dir = []
-                    for y, x in [[i+1, j], [i, j+1], [i-1, j], [i, j-1]]:
-                        if not (0 <= y < self.area_height and 0 <= x < self.area_width): continue
-                        if self.area[y][x] == -1: continue
-                        if abs(self.area[y][x] - self.area[i][j]) == 1:
-                            neighbor_dir.append([i2+(y-i), j2+(x-j)])
                     # 斜め四方: ポール配置
                     for i3, j3 in [[i2-1, j2-1],[i2-1, j2+1],[i2+1, j2-1],[i2+1, j2+1]]:
-                        if pole_area[i3][j3] == "O": continue
-                        pole_area[i3][j3] = "O"
-                        tmp.append([i3, j3])
+                        if self.pole_area[i3][j3] != " ": continue
+                        self.pole_area[i3][j3] = idx
+                        poles.append([i3, j3])
+                        idx += 1
+        return poles
 
-                    # 縦: ベルト接続
-                    flag = True
-                    for i3, j3 in [[i2, j2+1], [i2, j2-1]]:
-                        for ny, nx in neighbor_dir:
-                            if ny == i3 and nx == j3:
-                                pole_area[i3][j3] = " "
-                                flag = False
-                        if flag:
-                            if pole_area[i3][j3] == "|": continue
-                            pole_area[i3][j3] = "|"
-                        flag = True
-                    flag = True
-                    # 横: ベルト接続
-                    for i3, j3 in [[i2+1, j2], [i2-1, j2]]:
-                        for ny, nx in neighbor_dir:
-                            if ny == i3 and nx == j3:
-                                pole_area[i3][j3] = " "
-                                flag = False
-                        if flag:
-                            if pole_area[i3][j3] == "-": continue
-                            pole_area[i3][j3] = "-"
-                        flag = True
+    def create_pole_arrangement(self) -> list:
+        # ポール座標生成
+        self.pole_c = self.create_pole_coordinates()
 
-        # print("pole coordinates: ", *tmp)
+        # 拡張マップのスタート座標とゴール座標
+        start_c = [self.start[0]*2, self.start[1]*2+1]
+        goal_c = [self.goal[0]*2+2, self.goal[1]*2+1]
         
-        return pole_area
+        for i in range(self.area_height):
+            for j in range(self.area_width):
+                # 未探索なら
+                if self.area[i][j] != -1:
+                    i2 = i*2+1
+                    j2 = j*2+1
+
+                    """
+                    i, j: 経路マップの座標
+                    i2, j2: 拡張マップの座標
+                    """
+                    
+                    # 経路マップで隣接する座標の方向を保存
+                    neighbor_dir = [start_c, goal_c]
+                    for y, x in [[i+1, j], [i, j+1], [i-1, j], [i, j-1]]:
+                        # マップ範囲外、または未探索の座標を飛ばす
+                        if not (0 <= y < self.area_height and 0 <= x < self.area_width): continue
+                        if self.area[y][x] == -1: continue
+                        
+                        if abs(self.area[y][x] - self.area[i][j]) == 1:
+                            neighbor_dir.append([i2+(y-i), j2+(x-j)])
+
+                    # 縦横: ベルト
+                    flag = True
+                    # 右, 左, 上, 下
+                    for i3, j3 in [[i2, j2+1], [i2, j2-1], [i2+1, j2], [i2-1, j2]]:
+                        # 隣の座標かどうかを判定
+                        for ny, nx in neighbor_dir:
+                            if ny == i3 and nx == j3:
+                                flag = False
+                                break
+                        if flag:
+                            if self.pole_area[i3][j3] == "=": continue
+                            self.pole_area[i3][j3] = "="
+                        flag = True
+
+        # 無向グラフの隣接リスト    
+        adjacency_list = self.create_adjacency_list()
+        # 開始位置
+        sy1, sx1 = self.start[0]*2, self.start[1]*2
+        sy2, sx2 = self.start[0]*2, self.start[1]*2+2
+        pole_num1 = self.pole_area[sy1][sx1]
+        pole_num2 = self.pole_area[sy2][sx2]
+
+        self.create_belt_way(pole_num1, adjacency_list)
+        self.create_belt_way(pole_num2, adjacency_list)
+
+        self.show_pole_way()
+
+    def create_adjacency_list(self) -> list:
+        # 無向グラフの隣接リスト
+        adjacency_list = [set() for _ in range(len(self.pole_c))]
+        width = self.area_width*2+1
+        height = self.area_height*2+1
+
+        for idx, (py, px) in enumerate(self.pole_c):
+            # 上下方向探索, 上右下左
+            tmp = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+            for i, j in tmp:
+                y, x = py+i, px+j
+                # 範囲外は無視
+                if not (0 <= y < height and 0 <= x < width): continue
+
+                if self.pole_area[y][x] == "=":
+                    pole_idx = self.pole_area[py+i*2][px+j*2]
+                    adjacency_list[idx].add(pole_idx)
+                    adjacency_list[pole_idx].add(idx)
+                    
+        # print("Adjacency list", *adjacency_list)
+        return adjacency_list
+
+    def create_belt_way(self, p_num:int, adjacency_list:list) -> None:
+        Q = deque([p_num])
+        done_p = set([p_num])
+        while len(Q) > 0:
+            p = Q.popleft()
+            next_p_num = adjacency_list[p]
+
+            for i in next_p_num:
+                # i: 次の座標, p: 前の座標
+                if i in done_p: continue
+                # 接続方向, iとpの差分で決める
+                py, px = self.pole_c[p][0], self.pole_c[p][1]
+                iy, ix = self.pole_c[i]
+                y = (py-iy)//2
+                x = (px-ix)//2
+                self.pole_c[i].append([y, x])
+
+                done_p.add(i)
+                Q.append(i)
+            
+    def show_pole_way(self):
+        way = ["↑", "↓", "→", "←"]
+        tmp = ""
+        for pole in self.pole_c:
+            if len(pole) <= 2: continue
+            y, x = pole[0], pole[1]
+            wy, wx = pole[2]
+
+            if wy == -1: # 上
+                tmp = way[0]
+            elif wy == 1: # 下 
+                tmp = way[1]
+            elif wx == 1: # 右
+                tmp = way[2]
+            elif wx == -1: # 左
+                tmp = way[3]
+
+            self.pole_area[y+wy][x+wx] = tmp
--- make_map.py
+++ make_map.py
@@ -1,37 +1,35 @@
 import random
-from create_route import CreateRoute
-from visualize_map import visualize_map
+import time
 import numpy as np
-import pprint
+from create_route import CreateRoute
 
 
 # TODO: 2次元配列処理をnumpy配列にして高速化する
 # TODO: 障害物対応
 
 def main():
-    height, width = random.randint(3, 7), random.randint(3, 7)
-    # height, width = 3, 3
+    height, width = random.randint(5, 10), random.randint(5, 10)
     sy, sx = 0, random.randint(0, width-1)
     gy, gx = height-1, random.randint(sx, width-1)
-    # sy, sx = 0, 0
-    # gy, gx = 2, 0
     print("Map Size: ", "Height", height, "Width", width)
     print("start:", "sy", sy, "sx", sx)
     print("goal:", "gy", gy, "gx", gx)
     print("CongestionRate: 1.busy", "2.normal", "3.few")
 
-    congestion_rate = 1
+    congestion_rate = 2
     congestion_rate = int(input("Please Input Congestion Rate: "))
     if congestion_rate not in set([1, 2, 3]):
         print("Please input Congestion Rate within 1 to 3")
         return
     
+    pre = time.time()
+
     pole_route = CreateRoute(width, height, sy, sx, gy, gx)
     # 障害物生成
     # pole_route.create_obstacle(5)
     # 経過点生成
     passing_points = pole_route.make_passing_point(congestion_rate)
-    # 
+    
     for path in passing_points:
         start, goal = path
         # 幅優先探索
@@ -39,19 +37,19 @@ def main():
         # 経路復元と確定経路以外を一度リセット
         pole_route.reset_map(start, goal)
 
-    print("###########################################################")
+    print("Map Route ###########################################################")
     route_only = np.array(pole_route.area)
-    print("Map Route")
-    # print(route_only)
-    # viz_map = visualize_map(route_only, sy, sx, gy, gx, height, width)
-    # for row in viz_map:
-    #     print(*row)
+    print(route_only)
 
-    pole_area =  pole_route.create_pole_arrangement()
-    for row in pole_area:
+    print("Pole Arrangement ###########################################################")
+    pole_route.create_pole_arrangement()
+    for row in pole_route.pole_area:
         for i in row:
-            print(str(i).rjust(3), end="")
+            print(str(i).rjust(4), end="")
         print("")
+    
+    now = time.time()
+    print("実行時間: ", now-pre)
 
 if __name__ == "__main__":
     main()
